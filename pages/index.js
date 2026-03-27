a88b7dc7300967944760512784e852203383d1d8
onelpapa <dyzzang91@gmail.com>
2026-03-27 17:27:40 +0900

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Hello World! 👋</h1>
+      <p>Next.js 프로젝트입니다!</p>
+    </div>
+  )
+}
